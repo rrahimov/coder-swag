fd8d44a357d81744178defa4848ba92bf90e3b39
Ruhullah Rahimov <rahimov@Ruhullah-MacBook-Pro.local>
2021-01-09 01:10:54 +0400

    l-03

---
+++ coder-swag/coder-swag/Views/ProductCell.swift
@@ -0,0 +1,14 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Ruhullah Rahimov on 07.01.21.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productImage: UIImageView!
+}
